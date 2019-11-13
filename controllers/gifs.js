9db07be5f70c1e87d7dc4841a8404e4ae1d64cbb
Said Jillo <umurjillo@gmail.com>
2019-11-13 10:50:41 +0300

    added create gif image api

---
+++ controllers/gifs.js
@@ -0,0 +1,37 @@
+const Gif = require("../models/gifs");
+
+const gif = new Gif();
+
+exports.createGif = (req,res,next) =>{
+ 
+    let gif_obj = {
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        userId: req.body.userId
+    };
+
+
+    gif.save(gif_obj)
+        .then( (item)=>{
+            res.status(201).json({
+                status: "success",
+                data: {
+                    message: "GIF image successfully posted",
+                    articleId: item.articleid,
+                    createdOn: item.createdon,
+                    title: item.title,
+                    imageUrl: item.imageUrl,
+                }
+               
+            });
+        })
+
+        .catch( (error)=>{
+            res.status(400).json({
+                status: "error",
+                error: "GIF image could not be posted"                
+              })
+        });
+    
+ 
+};
