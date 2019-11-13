af7a7fa1beee6959dadb63328a42cb6dd1840340
Said Jillo <umurjillo@gmail.com>
2019-11-13 11:34:07 +0300

    added getAllGifs api function

---
+++ controllers/gifs.js
@@ -0,0 +1,56 @@
+const Gif = require("../models/gifs");
+
+const gif = new Gif();
+
+exports.createGif = (req,res,next) =>{
+ 
+    let gif_obj = {
+        title: req.body.title,
+        imageUrl: req.body.imageUrl,
+        userId: req.body.userId
+    };
+
+
+    gif.save(gif_obj)
+        .then( (item)=>{
+            res.status(201).json({
+                status: "success",
+                data: {
+                    message: "GIF image successfully posted",
+                    articleId: item.articleid,
+                    createdOn: item.createdon,
+                    title: item.title,
+                    imageUrl: item.imageUrl,
+                }
+               
+            });
+        })
+
+        .catch( (error)=>{
+            // error message
+            res.status(400).json({
+                status: "error",
+                error: "GIF image could not be posted"                
+              })
+        });
+    
+ 
+};
+
+
+exports.getAllGifs = (req,res,next)=>{
+
+    gif.find()
+
+        .then( (items)=>{
+
+        })
+
+        .catch( (error)=>{
+            res.status(400).json({
+                status: "error",
+                error: "GIF images could not be fetched."                
+              })
+        });
+
+};
